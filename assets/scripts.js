53ad2f25e72bd2c26cfb7a94f1dab691c8e1604b
Koppi Kolyvek <kk674@cornell.edu>
2021-07-04 14:55:32 -0700

    added icons to weather report and made search history tab functional

--- assets/scripts.js
+++ assets/scripts.js
@@ -23,7 +23,13 @@ if (!localStorage.getItem('searchHistory')) {
 function getCityData(api) {
     // USE FIRST API TO GET LATITUDE/LONGITUDE
     fetch(api)
-        .then(response => response.json())
+        // .then(response => response.json())
+        .then(function (response) {
+            if (response.status === 404) {
+                alert('Please enter a valid city!');
+            };
+            return response.json()
+        })
         .then(function (data) {
             // GRAB LATITUDE AND LONGITUDE FOR SECOND API CALL
             cityLat = data.coord.lat;
@@ -34,6 +40,15 @@ function getCityData(api) {
             
             // UPDATE PAGE WITH CITY NAME AND DATE
             showCityName(cityName);
+
+
+            // if api fetch worked, save to local storage
+            if (cityName) {
+                searchHistory.unshift(cityName);
+                localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+                showHistory(searchHistory);
+            };
+
 
             // GET FUTURE WEATHER DATA
             var weatherAPI = base + 'onecall?lat=' + cityLat + '&lon=' + cityLon + '&exclude=minutely,hourly&appid=' + key + '&units=imperial';
@@ -49,7 +64,7 @@ function getWeatherData(api) {
         .then(response => response.json())
         .then(function (data) {
             // CURRENT WEATHER
-            cityWeatherIcon = data.current.weather.icon;
+            cityWeatherIcon = data.current.weather[0].icon;
             cityTemp = data.current.temp;
             cityWind = data.current.wind_speed;
             cityHumidity = data.current.humidity;
@@ -70,6 +85,9 @@ function showCityName(city) {
 };
 
 function showCurrentWeather(icon, temp, wind, humidity, UV) {
+    // Update city with weather icon
+    var iconURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+    $('#currentCity').append('  <img src="' + iconURL + '"/>');
     // replace text of main current weather card with data from API
     $('#currentTemp').text('Temp: ' + temp + '°F');
     $('#currentWind').text('Wind: ' + wind + ' MPH');
@@ -81,6 +99,7 @@ function showCurrentWeather(icon, temp, wind, humidity, UV) {
         $('#currentUV').css('background-color', 'green');
     } else if (UV <= 7) {
         $('#currentUV').css('background-color', 'yellow');
+        $('#currentUV').css('color', 'black');
     } else {
         $('#currentUV').css('background-color', 'red');
     };
@@ -91,11 +110,13 @@ function showForecast(data) {
     $('#5day').text('5 Day Forecast:')
 
     for (var i=1; i<6; i++) {
+        var iconURL = 'http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '.png';
         var date = moment(data.daily[i].dt, 'X').format('MM/DD/YYYY')
         var temp = data.daily[i].temp.max;
         var wind = data.daily[i].wind_speed;
         var hum = data.daily[i].humidity;
 
+        $('#icon' + i).attr({'src':iconURL, 'alt':data.daily[i].weather[0].description});
         $('#date' + i).text(date);
         $('#temp' + i).text('Temp: ' + temp + '°F');
         $('#wind' + i).text('Wind: ' + wind + ' MPH');
@@ -103,15 +124,33 @@ function showForecast(data) {
     };
 };
 
+function showHistory(history) {
+    // empty existing list
+    $('#search-history').empty();
 
+    for (var i=0; i<history.length; i++) {
+        // create element
+        var wrapper = $('<a></a>');
+        var iconSpan = $('<span></span>');
+        var icon = $('<i></i>');
+
+        // add necessary styling
+        wrapper.addClass('panel-block');
+        iconSpan.addClass('panel-icon');
+        // icon.addClass('fas fa-map-marked-alt');
+        icon.attr({'class':'fas fa-map-marked-alt', 'aria-hidden':'true'});
+
+        // append element
+        iconSpan.append(icon);
+        wrapper.append(iconSpan);
+        wrapper.append(history[i]);
+        $('#search-history').append(wrapper);
+    };
+};
 
 // EVENT LISTENERS
 
 $('#search-city').on('click', function() {
-    // clear current search
-    // $('#currentWeather').empty();
-    // $('#futureForecast').empty();
-
     // parse the inputted city name and hit the API
     var searchTerm = $('#searchTerm').val();
 
@@ -121,10 +160,31 @@ $('#search-city').on('click', function() {
     
     var cityAPI = base + 'weather?q=' + searchTerm + '&appid=' + key + '&units=imperial';
     getCityData(cityAPI);
+
+    // clear search bar
+    $('#searchTerm').val('');
+});
+
+$('#search-history').on('click', function(event) {
+    if (event.target.matches('.panel-block')) {
+        // ping API again for whatever city is in the search history
+        var cityAPI = base + 'weather?q=' + event.target.text + '&appid=' + key + '&units=imperial';
+        getCityData(cityAPI);
+    };
 });
 
 $('#clear-history').on('click', function() {
     // empty search history div and clear local storage variable
     $('#search-history').empty();
+
+    // empty the history array and remove from local storage
+    searchHistory = [];
     localStorage.removeItem('searchHistory');
 });
+
+
+
+
+
+// display search history on load
+showHistory(searchHistory);
